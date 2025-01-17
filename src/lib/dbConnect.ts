788b6e42832c80b7aec5e37770e68a7db96adf94
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-01-17 11:51:27 +0530

    Resend email setup along with signup route

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -13,10 +13,11 @@ async function dbConnect(){
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || ' ', {})
+        const db = await mongoose.connect(process.env.MONGODB_URI || ' ', {}) // {} at the end denotes to use the default setting , but here we can use useUnifiedTopolgy or auth username or password , ssl : true and etc for modification of connection.
         console.log(db)
         connection.isConnected = db.connections[0].readyState
         console.log(connection.isConnected)
+        console.log(db.connections)
         console.log("Db connected successfully")
     } catch (error) {
         console.log("Db connection failed", error)
